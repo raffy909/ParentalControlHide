b2497303a0ba0664c3c5acba3beddff4157d8576
raffy909 <dapuzzo.raffaele@gmail.com>
2018-08-10 18:21:25 +0200

    loc

--- app/src/main/java/me/raffy909/parentalcontrolhide/ParentalControlHideService.java
+++ app/src/main/java/me/raffy909/parentalcontrolhide/ParentalControlHideService.java
@@ -1,8 +1,9 @@
 package me.raffy909.parentalcontrolhide;
 
 import android.accessibilityservice.AccessibilityService;
+import android.content.Intent;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
-
 
 public class ParentalControlHideService extends AccessibilityService {
     static final String TAG = "ParentalHideService";
@@ -11,16 +12,16 @@ public class ParentalControlHideService extends AccessibilityService {
     public void onAccessibilityEvent(AccessibilityEvent event) {
         String msg = getString(R.string.app_restricted_message);
 
-        if(event.getEventType()==AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+        Log.i(TAG, msg);
+
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
             String name = event.getClassName().toString();
             String text = event.getText().toString();
 
-            //Log.i(TAG, name + ':' + text);
-            if(name.contains("android.app.AlertDialog"))
-                if(text.contains(msg))
+            if (name.contains("android.app.AlertDialog"))
+                if (text.contains(msg))
                     performGlobalAction(GLOBAL_ACTION_BACK);
         }
-
     }
 
     @Override
